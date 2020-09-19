7af2030fa99e4a52c8f5e559432e0435a8997001
Abimael Rodrigues Sergio <abimaelr.sergio@gmail.com>
2020-09-19 02:24:20 -0300

    atualizar

--- forca.cpp
+++ forca.cpp
@@ -2,10 +2,13 @@
 #include <string>
 #include <map>
 #include <vector>
+#include <fstream>
+#include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
-const string PALAVRA_SECRETA = "MELANCIA";
+string palavra_secreta = "MELANCIA";
 map<char, bool> chutou;
 vector<char> chutes_errados;
 
@@ -16,10 +19,14 @@ void imprimir_cabecalho();
 void imprimir_erros();
 void imprimir_palavra();
 void chutar();
+vector<string> ler_arquivo();
+void sortear_palavra();
 
 int main(int argc, char const *argv[])
 {
     imprimir_cabecalho();
+
+    sortear_palavra();
 
     while (nao_acertou() && nao_enforcou())
     {
@@ -30,7 +37,7 @@ int main(int argc, char const *argv[])
         chutar();
     }
     cout << "FIM DO JOGO!" << endl;
-    cout << "A palavra secreta era: " << PALAVRA_SECRETA << endl;
+    cout << "A palavra secreta era: " << palavra_secreta << endl;
     if (nao_acertou())
     {
         cout << "PERDEU!! \n Tente novamente!" << endl;
@@ -47,7 +54,7 @@ bool letra_existe(char chute)
 {
     // For melorado do C++11
     // compilar com: g++ forca.cpp -o forca.out -std=c++11
-    for (char letra : PALAVRA_SECRETA)
+    for (char letra : palavra_secreta)
     {
         if (chute == letra)
         {
@@ -59,7 +66,7 @@ bool letra_existe(char chute)
 
 bool nao_acertou()
 {
-    for (char letra : PALAVRA_SECRETA)
+    for (char letra : palavra_secreta)
     {
         if (!chutou[letra])
         {
@@ -94,7 +101,7 @@ void imprimir_erros()
 
 void imprimir_palavra()
 {
-    for (char letra : PALAVRA_SECRETA)
+    for (char letra : palavra_secreta)
     {
         if (chutou[letra])
         {
@@ -122,4 +129,28 @@ void chutar()
         chutes_errados.push_back(chute);
     }
     cout << endl;
+}
+
+vector<string> ler_arquivo()
+{
+    ifstream arquivo;
+    arquivo.open("palavras.txt");
+    int quantidade_palavras;
+    arquivo >> quantidade_palavras;
+    vector<string> palavras_do_arquivo;
+    for (int i = 0; i < quantidade_palavras; i++)
+    {
+        string palavra_lida;
+        arquivo >> palavra_lida;
+        palavras_do_arquivo.push_back(palavra_lida);
+    }
+    return palavras_do_arquivo;
+}
+
+void sortear_palavra()
+{
+    vector<string> palavras = ler_arquivo();
+    srand(time(NULL));
+    int indice_sorteado = rand() % palavras.size();
+    palavra_secreta = palavras[indice_sorteado];
 }
