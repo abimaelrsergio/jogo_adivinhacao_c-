3a8b4ce570ec5f8529ec70a4f63b00a8f7ec9142
Abimael Rodrigues Sergio <abimaelr.sergio@gmail.com>
2020-09-19 03:03:08 -0300

    ATUALIZAÇÃO

--- forca.cpp
+++ forca.cpp
@@ -21,6 +21,8 @@ void imprimir_palavra();
 void chutar();
 vector<string> ler_arquivo();
 void sortear_palavra();
+void adicionar_palavra();
+void salvar_arquivo(vector<string> lista_palavras);
 
 int main(int argc, char const *argv[])
 {
@@ -45,6 +47,13 @@ int main(int argc, char const *argv[])
     else
     {
         cout << "PARABÉNS !!! \n Você acertou a palavra secreta." << endl;
+        cout << "Você deseja adicionar uma nova palavra no banco de palavras? (S/N) ";
+        char resposta;
+        cin >> resposta;
+        if (resposta == 'S' || resposta == 's')
+        {
+            adicionar_palavra();
+        }
     }
 
     return 0;
@@ -120,7 +129,7 @@ void chutar()
     cout << "Seu chute: ";
     char chute;
     cin >> chute;
-    chutou[chute] = true;
+    chutou[toupper(chute)] = true;
 
     cout << "O seu chute foi: " << chute << endl;
 
@@ -135,16 +144,25 @@ vector<string> ler_arquivo()
 {
     ifstream arquivo;
     arquivo.open("palavras.txt");
-    int quantidade_palavras;
-    arquivo >> quantidade_palavras;
-    vector<string> palavras_do_arquivo;
-    for (int i = 0; i < quantidade_palavras; i++)
+    if (arquivo.is_open())
     {
-        string palavra_lida;
-        arquivo >> palavra_lida;
-        palavras_do_arquivo.push_back(palavra_lida);
+        int quantidade_palavras;
+        arquivo >> quantidade_palavras;
+        vector<string> palavras_do_arquivo;
+        for (int i = 0; i < quantidade_palavras; i++)
+        {
+            string palavra_lida;
+            arquivo >> palavra_lida;
+            palavras_do_arquivo.push_back(palavra_lida);
+        }
+        arquivo.close();
+        return palavras_do_arquivo;
     }
-    return palavras_do_arquivo;
+    else
+    {
+        cout << "Nao foi possível acessar o banco de palavras." << endl;
+        exit(0);
+    }
 }
 
 void sortear_palavra()
@@ -153,4 +171,34 @@ void sortear_palavra()
     srand(time(NULL));
     int indice_sorteado = rand() % palavras.size();
     palavra_secreta = palavras[indice_sorteado];
+}
+
+void adicionar_palavra()
+{
+    cout << "Digite a nova palavra, usando letras maiúsculas: ";
+    string nova_palavra;
+    cin >> nova_palavra;
+    vector<string> lista_palavras = ler_arquivo();
+    lista_palavras.push_back(nova_palavra);
+    salvar_arquivo(lista_palavras);
+}
+
+void salvar_arquivo(vector<string> lista_palavras)
+{
+    ofstream arquivo;
+    arquivo.open("palavras.txt");
+    if (arquivo.is_open())
+    {
+        arquivo << lista_palavras.size() << endl;
+        for (string palavra : lista_palavras)
+        {
+            arquivo << palavra << endl;
+        }
+        arquivo.close();
+    }
+    else
+    {
+        cout << "Não fo possível acessar o banco de palavras." << endl;
+        exit(0);
+    }
 }
