4bd2dd39e7b2a9a3319ab8b5729bc9b14258e28a
Abimael Rodrigues Sergio <abimaelr.sergio@gmail.com>
2020-09-18 23:21:12 -0300

    atualização

--- jogo-advinhacao.cpp
+++ jogo-advinhacao.cpp
@@ -10,10 +10,13 @@ int main(int argc, char const *argv[])
     const int NUMERO_SECRETO = 42;
 
     bool nao_acertou = true;
+    int tentativas = 0;
 
     while (nao_acertou)
     {
+        tentativas++;
         int chute;
+        cout << "Tentativas " << tentativas << endl;
         cout << "Qual seu chute? ";
         cin >> chute;
         cout << "O valor do seu chute é: " << chute << endl;
@@ -34,8 +37,10 @@ int main(int argc, char const *argv[])
         {
             cout << "Seu chute foi menor que o número secreto!" << endl;
         }
+        cout << "-----------------------------------------" << endl;
     }
-    cout << "Fim do jogo!";
+    cout << "Você acertou o numero secreto em " << tentativas << " tentativas." << endl;
+    cout << "Fim do jogo!" << endl;
 
     return 0;
 }
